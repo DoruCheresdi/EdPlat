7e9c8830fa08532c5d75e7c72a76543776e3e733
Doru Cheresdi <doru.cheresdi@yahoo.com>
2022-08-31 22:21:05 +0300

    Added sanity check test

--- src/test/java/edplatform/edplat/EdPlatApplicationTests.java
+++ src/test/java/edplatform/edplat/EdPlatApplicationTests.java
@@ -1,13 +1,22 @@
 package edplatform.edplat;
 
+import edplatform.edplat.controllers.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EdPlatApplicationTests {
 
+    @Autowired
+    private UserController userController;
+
+    // sanity check test:
     @Test
     void contextLoads() {
+        assertThat(userController).isNotNull();
     }
 
 }
