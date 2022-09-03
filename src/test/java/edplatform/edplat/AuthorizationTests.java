35af446c484a8e2fbb8e538b46959fa146e45f77
Doru Cheresdi <doru.cheresdi@yahoo.com>
2022-09-03 05:44:37 +0300

    Added authorization test for new assignments

--- src/test/java/edplatform/edplat/AuthorizationTests.java
+++ src/test/java/edplatform/edplat/AuthorizationTests.java
@@ -1,14 +1,19 @@
 package edplatform.edplat;
 
+import edplatform.edplat.entities.users.CustomUserDetails;
+import edplatform.edplat.entities.users.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,11 +22,20 @@ public class AuthorizationTests {
     @Autowired
     private MockMvc mvc;
 
-    // "/assignment/new" - needs "course-{id}-owner" authority
     @Test
-    void ShouldRedirectToLoginPageNewAssignmentUnauthenticated() throws Exception {
-        mvc.perform(get("/assignment/new"))
+    @DisplayName("\"/assignment/new\" - authentication test")
+    void shouldRedirectToLoginPageNewAssignmentUnauthenticated() throws Exception {
+        mvc.perform(get("/assignment/new").param("courseId", "3"))
                 .andExpect(status().is3xxRedirection()) // redirect (302)
                 .andExpect(redirectedUrl("http://localhost/login")); // redirect URL
+    }
+
+    @Test
+    @DisplayName("\"/assignment/new\" - needs \"course-{id}-owner\" authority")
+    void shouldAuthorizeNewAssignment() throws Exception {
+        mvc.perform(get("/assignment/new").param("courseId", "3")
+                        .with(user("testUser").password("pass")
+                                .authorities(new SimpleGrantedAuthority("course-3-owner"))))
+                .andExpect(view().name("add_assignment"));
     }
 }
