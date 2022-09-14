c7e9e958f23866cb70f5ae7870358645c7bac2d0
Doru Cheresdi <doru.cheresdi@yahoo.com>
2022-09-14 17:10:07 +0300

    Tidied up code

--- src/test/java/edplatform/edplat/AuthorizationTests.java
+++ src/test/java/edplatform/edplat/AuthorizationTests.java
@@ -3,17 +3,12 @@ package edplatform.edplat;
 import edplatform.edplat.entities.authority.Authority;
 import edplatform.edplat.entities.users.CustomUserDetails;
 import edplatform.edplat.entities.users.User;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -28,16 +23,6 @@ public class AuthorizationTests {
 
     @Autowired
     private MockMvc mvc;
-
-    @BeforeEach
-    void beforeEach() {
-
-    }
-
-    @AfterEach
-    void afterEach() {
-
-    }
 
     @Test
     @DisplayName("\"/assignment/new\" - needs \"course-{id}-owner\" authority")
