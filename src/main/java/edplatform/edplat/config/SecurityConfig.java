75fb491473606dedcd9ea0e146461afbcb699609
Doru Cheresdi <doru.cheresdi@yahoo.com>
2022-10-14 13:57:57 +0300

    Course owners are no longer able to enroll in their own courses

--- src/main/java/edplatform/edplat/config/SecurityConfig.java
+++ src/main/java/edplatform/edplat/config/SecurityConfig.java
@@ -43,6 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // requires authentication for every request except for the login page:
         http.authorizeRequests()
+                .mvcMatchers("/course/enroll")
+                    .access("not @securityAuthorizationChecker.checkCourseOwnerByCourseId(authentication,request)")
                 .mvcMatchers("/assignment/new", "/course/delete")
                     .access("@securityAuthorizationChecker.checkCourseOwnerByCourseId(authentication,request)")
                 .mvcMatchers("/course/edit", "/course/process_img_edit",
